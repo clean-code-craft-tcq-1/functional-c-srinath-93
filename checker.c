eece63e6fa20deb367a7caa6cc5e642e84cece93
srinath-93 <77610922+srinath-93@users.noreply.github.com>
2021-03-03 05:17:17 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -10,10 +10,17 @@ int main()
   assert(batteryHealth(190, 70, 0.7,PUBLIC));
   assert(batteryHealth(140, 70, 0.7,EV));
   
-  assert(batteryHealth(45, 85, 0,GENERAL));
-  assert(batteryHealth(100, 85, 0,HOME));
-  assert(batteryHealth(210, 85, 0,PUBLIC));
-  assert(batteryHealth(150, 85, 0,EV));
+  batteryHealth(45, 85, 0,GENERAL);
+  batteryHealth(100, 85, 0,HOME);
+  batteryHealth(210, 85, 0,PUBLIC);
+  batteryHealth(150, 85, 0,EV);
+  
+   /* Positive test case - BMS Passed */
+//  assert(!batteryHealth(25, 85, 0,GENERAL));
+//  assert(!batteryHealth(80, 85, 0,HOME));
+//  assert(!batteryHealth(190, 85, 0,PUBLIC));
+//  assert(!batteryHealth(140, 85, 0,EV));
+  
   /* Positive test case - BMS Passed */
 //  assert(!batteryHealth(45, 85, 0,GENERAL));
 //  assert(!batteryHealth(100, 85, 0,HOME));
