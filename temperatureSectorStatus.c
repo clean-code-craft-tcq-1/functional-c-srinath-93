b5cc0b87214212655e014c017d413621923f435c
srinath-93 <77610922+srinath-93@users.noreply.github.com>
2021-03-03 04:59:41 +0530

    Update temperatureSectorStatus.c

--- temperatureSectorStatus.c
+++ temperatureSectorStatus.c
@@ -4,34 +4,26 @@ int temperatureSectorStatus(float temperature,enum MajorSector sectorType)
 {
 	int retStatus = 1;
 	/* choose respective sector */
-	if(temperature>(float)TEMP_MIN)
+	switch(sectorType)
 	{
-		switch(sectorType)
-		{
-			case GENERAL:
-			retStatus = temperatureStatus(temperature, (float)TEMP_GENRAL_MAX);
-			break;
-			case HOME:
-			retStatus = temperatureStatus(temperature, (float)TEMP_HOME_MAX);
-			break;
-			case PUBLIC:
-			retStatus = temperatureStatus(temperature, (float)TEMP_PUBLIC_MAX);
-			break;	
-			case EV:
-			retStatus = temperatureStatus(temperature, (float)TEMP_EV_MAX);
-			break;
-                	default:
-                	{
-			printf("Please choose a proper sector!\n");
-			retStatus = 0;
-	        	}
-	        	break;
-		}/* End of switch */
-	}
-	else
-	{
-		printf("Required temperature is below the minimum range\n");
+		case GENERAL:
+		retStatus = temperatureStatus(temperature, (float)TEMP_GENRAL_MAX);
+		break;
+		case HOME:
+		retStatus = temperatureStatus(temperature, (float)TEMP_HOME_MAX);
+		break;
+		case PUBLIC:
+		retStatus = temperatureStatus(temperature, (float)TEMP_PUBLIC_MAX);
+		break;	
+		case EV:
+		retStatus = temperatureStatus(temperature, (float)TEMP_EV_MAX);
+		break;
+                default:
+                {
+		printf("Please choose a proper sector!\n");
 		retStatus = 0;
-	}
+	        }
+	        break;
+	}/* End of switch */
 	return retStatus;
 }
