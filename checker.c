7ad390db057a8290b6563e136d87f958ef178d4a
srinath-93 <77610922+srinath-93@users.noreply.github.com>
2021-03-03 06:27:55 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -4,7 +4,7 @@
 
 int main() 
 {
-  /* Negative test case - BMS issue detected */
+  /* Negative test case - BMS issue detected (Temperature, Soc and Charge rate is not in expected range) */
   assert(batteryHealth(25, 70, 0.7,GENERAL));
   assert(batteryHealth(80, 70, 0.7,HOME));
   assert(batteryHealth(190, 70, 0.7,PUBLIC));
@@ -16,15 +16,10 @@ int main()
 //  batteryHealth(210, 85, 0,PUBLIC);
 //  batteryHealth(150, 85, 0,EV);
   
-   /* Positive test case - BMS Passed */
+   /* Positive test case - BMS Passed (Temperature, Soc and Charge rate is within expected range) */
   assert(!batteryHealth(25, 85, 0,GENERAL));
   assert(!batteryHealth(80, 85, 0,HOME));
   assert(!batteryHealth(190, 85, 0,PUBLIC));
   assert(!batteryHealth(140, 85, 0,EV));
   
-  /* Positive test case - BMS Passed */
-//  assert(!batteryHealth(45, 85, 0,GENERAL));
-//  assert(!batteryHealth(100, 85, 0,HOME));
-//  assert(!batteryHealth(210, 85, 0,PUBLIC));
-//  assert(!batteryHealth(150, 85, 0,EV));
 }
