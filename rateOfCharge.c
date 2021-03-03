cd47f272c89da9f04614b46243c19cf0863e6bd7
srinath-93 <77610922+srinath-93@users.noreply.github.com>
2021-03-03 06:53:25 +0530

    Update rateOfCharge.c

--- rateOfCharge.c
+++ rateOfCharge.c
@@ -1,10 +1,19 @@
 #include "checker_prv.h"
 
+/*
+ **********************************************************************************************************************
+ * Service name         : rateOfCharge
+ * Syntax               : int rateOfCharge(float chargeRate) 
+ * param[in]            : chargeRate
+ * return               : 0 - out of range, 1 - in range
+ * Description          : Program to check if Charge Rate is out of range
+ **********************************************************************************************************************
+ */
 int rateOfCharge(float chargeRate)
 {
   if(chargeRate > 0.8)
   {
-    printf("Charge Rate out of range!\n");
+    printf("Charge Rate(%f) out of range(%f)!\n",chargeRate,(float)RATEOFCHARGE);
     return 0;
   }
   return 1;
