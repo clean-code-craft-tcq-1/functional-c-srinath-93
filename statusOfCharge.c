aa1793f9a61b897948e153eb1a1fabdd3e00f448
srinath-93 <77610922+srinath-93@users.noreply.github.com>
2021-03-03 06:57:47 +0530

    Update statusOfCharge.c

--- statusOfCharge.c
+++ statusOfCharge.c
@@ -1,10 +1,19 @@
 #include "checker_prv.h"
 
+/*
+ **********************************************************************************************************************
+ * Service name         : statusOfCharge
+ * Syntax               : int statusOfCharge(float stateofcharge)
+ * param[in]            : stateofcharge
+ * return               : 0 - out of range, 1 - in range
+ * Description          : Program to check if Charge Rate is out of range
+ **********************************************************************************************************************
+ */
 int statusOfCharge(float stateofcharge)
 {
-  if(stateofcharge < 20 || stateofcharge > 80)
+  if(stateofcharge < SOC_MIN || stateofcharge > SOC_MAX)
   {
-    printf("State of Charge out of range!\n\n");
+    printf("State of Charge(%f) is not with the expected range(%f - %f)!\n\n",stateofcharge,SOC_MIN,SOC_MAX);
     return 0;
   }
   return 1;
