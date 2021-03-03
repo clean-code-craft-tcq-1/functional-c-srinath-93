60e1cf91a3080a7b9524dc5753e6e0738ee2040a
srinath-93 <77610922+srinath-93@users.noreply.github.com>
2021-03-03 06:07:49 +0530

    Update statusOfCharge.c

--- statusOfCharge.c
+++ statusOfCharge.c
@@ -2,7 +2,7 @@
 
 int statusOfCharge(float stateofcharge)
 {
-  if(soc < 20 || soc > 80)
+  if(stateofcharge < 20 || stateofcharge > 80)
   {
     printf("State of Charge out of range!\n\n");
     return 0;
