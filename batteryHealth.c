8dd186cc2fda6f0698eb18a90afeb2f58e4d6db0
srinath-93 <77610922+srinath-93@users.noreply.github.com>
2021-03-03 06:06:04 +0530

    Update batteryHealth.c

--- batteryHealth.c
+++ batteryHealth.c
@@ -2,11 +2,6 @@
 
 int batteryHealth(float temperature, float soc, float chargeRate, enum MajorSector sector)
 {
-  if(((temperature>(float)TEMP_MIN)&&(temperatureSectorStatus(temperature,sector)) == 0))
-  {
-    printf("Provided input is out of range!\n");
-    return 0;
-  } 
   return ((temperature>(float)TEMP_MIN) 
 	  && (temperatureSectorStatus(temperature,sector))
 	  && (statusOfCharge(soc)) && (rateOfCharge(chargeRate)));
