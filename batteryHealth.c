4647589a0c1ad36145fa90d0f645ae8721cfdda2
srinath-93 <77610922+srinath-93@users.noreply.github.com>
2021-03-03 06:32:26 +0530

    Update batteryHealth.c

--- batteryHealth.c
+++ batteryHealth.c
@@ -1,5 +1,16 @@
 #include "checker_prv.h"
-
+/*
+ **********************************************************************************************************************
+ * Service name         : batteryHealth
+ * Syntax               : int batteryHealth(float temperature, float soc, float chargeRate, enum MajorSector sector)
+ * param[in]            : temperature
+ * param[in]            : soc
+ * param[in]            : chargeRate
+ * param[in]            : sector
+ * return               : 0, 1
+ * Description          : Program to verify battery health based on temperature, state of charge and charge rate
+ **********************************************************************************************************************
+ */
 int batteryHealth(float temperature, float soc, float chargeRate, enum MajorSector sector)
 {
   return ((temperatureSectorStatus(temperature,sector))
