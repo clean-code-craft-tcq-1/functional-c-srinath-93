bb18333066874c0c39dd17419b49b00ffa32d092
srinath-93 <77610922+srinath-93@users.noreply.github.com>
2021-03-03 06:16:43 +0530

    Update temperatureStatus.c

--- temperatureStatus.c
+++ temperatureStatus.c
@@ -4,7 +4,7 @@ int temperatureStatus(float temperature, float cmprTemperature)
 {
 	int retStatus = 1;
 	/* Check if the temperature is out of range */
-	if(temperature > cmprTemperature)
+	if((temperature>(float)TEMP_MIN) && (temperature > cmprTemperature))
 	{
 		retStatus = 0;
                 printf("Selected temperature has breached the max range!\n");
