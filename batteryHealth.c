046636e780ba24717a8a764b514a9480dcda75ee
srinath-93 <77610922+srinath-93@users.noreply.github.com>
2021-03-11 03:44:44 +0530

    Update batteryHealth.c

--- batteryHealth.c
+++ batteryHealth.c
@@ -7,13 +7,14 @@
  * param[in]            : soc
  * param[in]            : chargeRate
  * param[in]            : sector
+ * param[in]            : temperatureUnit
  * return               : 0, 1
  * Description          : Program to verify battery health based on temperature, state of charge and charge rate
  **********************************************************************************************************************
  */
-int batteryHealth(float temperature, float soc, float chargeRate, enum MajorSector sector)
+int batteryHealth(float temperature, float soc, float chargeRate, enum MajorSector sector, int temperatureUnit)
 {
-  return ((temperatureSectorStatus(temperature,sector))
+  return ((temperatureSectorStatus(temperature,sector,temperatureUnit))
 	  && (statusOfCharge(soc)) && (rateOfCharge(chargeRate)));
 }
 	
