ec90e0b839cba51475066c65c163e7db7dec5c1d
srinath-93 <77610922+srinath-93@users.noreply.github.com>
2021-03-11 04:24:55 +0530

    Update alertStatus.c

--- alertStatus.c
+++ alertStatus.c
@@ -1,8 +1,14 @@
 #include "checker_prv.h"
 
-void warningsGenerator(float breachedValue, toleranceRange_st warnType)
+void warningsGenerator(float breachedValue, toleranceRange_st warnType, int tempUnit)
 {
-	char warnMessage[MAX_WARNING_TEXT_CHARS];
-	ParamToString(warnMessage,warnType);
-	printf(" Breached value - %f \t %s \n",breachedValue, warnMessage);
+  float convTemp = 0.0;
+  char warnMessage[MAX_WARNING_TEXT_CHARS];
+  if(tempUnit == FAHR)
+  {
+	  convTemp = CELTOFAHR(breachedValue);
+	  printf("Requested temperature in Fahrenhiet %f \n",convTemp);
+  }
+  ParamToString(warnMessage,warnType);
+  printf(" Breached value - %f \t %s \n",breachedValue, warnMessage);
 }
