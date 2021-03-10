f318274ed3cff5eb56257344087025aeba41303d
srinath-93 <77610922+srinath-93@users.noreply.github.com>
2021-03-11 04:01:10 +0530

    Update temperatureSectorStatus.c

--- temperatureSectorStatus.c
+++ temperatureSectorStatus.c
@@ -4,20 +4,21 @@ int temperatureMax[]={TEMP_GENRAL_MAX, TEMP_HOME_MAX, TEMP_PUBLIC_MAX, TEMP_EV_M
 /*
  **********************************************************************************************************************
  * Service name         : temperatureSectorStatus
- * Syntax               : int temperatureSectorStatus(float temperature,enum MajorSector sectorType) 
+ * Syntax               : int temperatureSectorStatus(float temperature,enum MajorSector sectorType, int temperatureUnit) 
  * param[in]            : temperature
  * param[in]            : sectorType
+ * param[in]            : temperatureUnit
  * return               : 0 - out of range, 1 - in range
  * Description          : Program to check if requested sector is valid and to identify the max sector range
  **********************************************************************************************************************
  */
-int temperatureSectorStatus(float temperature,enum MajorSector sectorType) 
+int temperatureSectorStatus(float temperature,enum MajorSector sectorType, int temperatureUnit) 
 {
 	int retStatus = 1;
 	/* choose respective sector */
 	if(sectorType<((int)MAXNUMBEROFSECTORS))
 	{
-		retStatus = temperatureStatus(temperature, (float)temperatureMax[sectorType]);
+		retStatus = temperatureStatus(temperature, (float)temperatureMax[sectorType],temperatureUnit);
 	}
 	else
 	{
