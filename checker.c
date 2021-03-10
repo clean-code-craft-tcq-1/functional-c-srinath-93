304c01718c451994481e71bb3f24a64631126304
srinath-93 <77610922+srinath-93@users.noreply.github.com>
2021-03-11 03:48:19 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -5,22 +5,22 @@
 int main() 
 {
   /* Negative test case - BMS issue detected (Temperature, Soc and Charge rate is not in expected range) */
-  assert(batteryHealth(25, 70, 0.7,GENERAL));
-  assert(batteryHealth(80, 70, 0.7,HOME));
-  assert(batteryHealth(190, 70, 0.7,PUBLIC));
-  assert(batteryHealth(140, 70, 0.7,EV));
+  assert(batteryHealth(25, 70, 0.7,GENERAL,CEL));
+  assert(batteryHealth(80, 70, 0.7,HOME,FAHR));
+  assert(batteryHealth(190, 70, 0.7,PUBLIC,CEL));
+  assert(batteryHealth(140, 70, 0.7,EV,FAHR));
   
   /* Positive test case - BMS Passed (Temperature, Soc and Charge rate is within expected range) */
-  assert(!batteryHealth(25, 85, 0,GENERAL));
-  assert(!batteryHealth(80, 85, 0,HOME));
-  assert(!batteryHealth(190, 85, 0,PUBLIC));
-  assert(!batteryHealth(140, 85, 0,EV));
+  assert(!batteryHealth(25, 85, 0,GENERAL,CEL));
+  assert(!batteryHealth(80, 85, 0,HOME,FAHR));
+  assert(!batteryHealth(190, 85, 0,PUBLIC,CEL));
+  assert(!batteryHealth(140, 85, 0,EV,FAHR));
   
   /* to check function response */
-  batteryHealth(25, 70, 0,GENERAL);     /* Temperature in range */
-  batteryHealth(55, 85, 0,HOME);        /* soc out of range */
-  batteryHealth(55, 70, 0.9,PUBLIC);    /* charge rate not in range */
-  batteryHealth(45, 70, 0,EV);          /* everything in range */
-  batteryHealth(45, 70, 0,(int)5);      /* Sector out of range */
+  batteryHealth(25, 70, 0,GENERAL,CEL);     /* Temperature in range */
+  batteryHealth(55, 85, 0,HOME,FAHR);        /* soc out of range */
+  batteryHealth(55, 70, 0.9,PUBLIC,CEL);    /* charge rate not in range */
+  batteryHealth(45, 70, 0,EV,FAHR);          /* everything in range */
+  batteryHealth(45, 70, 0,(int)5,CEL);      /* Sector out of range */
   
 }
