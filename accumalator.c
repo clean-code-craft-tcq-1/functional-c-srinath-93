2f84a030f8e9e9a7fa20d79ffd630c3ba0b016ec
srinath-93 <77610922+srinath-93@users.noreply.github.com>
2021-03-18 02:46:17 +0530

    Update accumalator.c

--- accumalator.c
+++ accumalator.c
@@ -17,21 +17,31 @@ void dataAccumalator(float dataInput,toleranceRange_st range)
 {
   switch(range.paramType)
   {
-      
-      if(dataRange.breachMinType == MIN)
+    case TEMPERATURE:
       {
-        case TEMPERATURE: dataRange.tempInputParam = dataInput; dataRange.tempMinBreach = range.minTolerance; break;
-        case SOC:         dataRange.socInputParam = dataInput; dataRange.socMinBreach = range.minTolerance; break;
-        case ROC:         dataRange.rocInputParam = dataInput; dataRange.rocMinBreach = range.minTolerance; break;
-        default: /* Do nothing */ break;
+        dataRange.tempInputParam = dataInput;
+        dataRange.tempMinBreach = range.minTolerance;
+        dataRange.tempMaxBreach = range.maxTolerance;
       }
-      else
+      break;
+    case SOC:
       {
-        case TEMPERATURE: dataRange.tempInputParam = dataInput; dataRange.tempMaxBreach = range.maxTolerance; break;
-        case SOC:         dataRange.socInputParam = dataInput; dataRange.socMaxBreach = range.maxTolerance; break;
-        case ROC:         dataRange.rocInputParam = dataInput; dataRange.rocMaxBreach = range.maxTolerance; break;
-        default: /* Do nothing */ break;
+        dataRange.socInputParam = dataInput; 
+        dataRange.socMinBreach = range.minTolerance;
+        dataRange.socMaxBreach = range.maxTolerance;
       }
-          
+      break;
+    case ROC:
+      {
+        dataRange.rocInputParam = dataInput;
+        dataRange.rocMinBreach = range.minTolerance;
+        dataRange.rocMaxBreach = range.maxTolerance;
+      }
+      break;
+    default:
+      {
+        /* Do nothing */
+      }
+      break;
   }
 }
