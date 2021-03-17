65eb8f9b833d5cb0dbcd36ff91ea19e3e48660a3
srinath-93 <77610922+srinath-93@users.noreply.github.com>
2021-03-18 02:13:24 +0530

    Update accumalate_Report_Prv.h

--- accumalate_Report_Prv.h
+++ accumalate_Report_Prv.h
@@ -7,18 +7,18 @@
 /* Structure */
 typedef struct
 {
-  int parameterType;
-  float tempInputParam;
+	int parameterType;
+	float tempInputParam;
 	float tempMinBreach;
 	float tempMaxBreach;
-  float socInputParam;
+	float socInputParam;
 	float socMinBreach;
 	float socMaxBreach;
-  float rocInputParam;
+	float rocInputParam;
 	float rocMinBreach;
 	float rocMaxBreach;
-	int breachMinRange; /* can be maintained as 1 byte integer in future update */
-  int breachMaxRange; /* can be maintained as 1 byte integer in future update */
+	int breachMinType; /* can be maintained as 1 byte integer in future update */
+	int breachMaxType; /* can be maintained as 1 byte integer in future update */
 }accumalateRange_st;
 
 #endif /* ACCUMALATE_REPORT_PRV_H_ */
