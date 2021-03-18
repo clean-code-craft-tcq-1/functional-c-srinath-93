d605c4da671f08f8b853f3f89643631a01d27f9d
srinath-93 <77610922+srinath-93@users.noreply.github.com>
2021-03-18 13:56:14 +0530

    Update alertStatus.c

--- alertStatus.c
+++ alertStatus.c
@@ -16,13 +16,16 @@ void warningsGenerator(toleranceRange_st warnType)
   printf(" Breached value - %f \t %s \n",warnType.inputParam, warnMessage);
 }
 
-void controllerAction()
+unsigned int controllerAction()
 {
+//	unsigned int retStatus = 1;
 	accumalateRange_st cntrlrAction = dataRange;
 	printf("\n\n\nBreached type - %d \n",cntrlrAction.parameterType);
 	/* cntrlAction can be used for further action like - email alert, new feauture addition, etc */
 	if(dataRange.retstatus < 1)
 	{
 		accumateReportCnt++; /* increments when some breach has occured */
+//		retStatus = 0;
 	}
+	return accumateReportCnt;
 }
